1536abefa993a447aa618c8d4f005bd76c4557eb
TelmanYusifov <43857129+TelmanYusifov@users.noreply.github.com>
2020-12-24 13:34:24 +0400

    MultiLanguage Web App is ready.

---
+++ MultiLanguageWebApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguageWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string language)
+        {
+            if (!string.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie("Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MultiLanguageWebApp/Controllers/LanguageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguageWebApp.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index(string language)
+        {
+            if (!string.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie("Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+    }
+}
